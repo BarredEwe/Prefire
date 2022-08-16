b78afd1caa89bd9ca31d760f2bfc0ceede8caef9
Maksim Grishutin <mgrishutin@ozon.ru>
2022-08-16 17:34:58 +0300

    Refactor

--- Shared/PlaybookView.swift
+++ Shared/PlaybookView.swift
@@ -204,21 +204,3 @@ extension View {
         }
     }
 }
-
-private struct SafeAreaInsetsKey: EnvironmentKey {
-    static var defaultValue: EdgeInsets {
-        (UIApplication.shared.connectedScenes.first as? UIWindowScene)?.windows.first?.safeAreaInsets.swiftUiInsets ?? EdgeInsets()
-    }
-}
-
-public extension EnvironmentValues {
-    var safeAreaInsets: EdgeInsets {
-        self[SafeAreaInsetsKey.self]
-    }
-}
-
-private extension UIEdgeInsets {
-    var swiftUiInsets: EdgeInsets {
-        EdgeInsets(top: top, leading: left, bottom: bottom, trailing: right)
-    }
-}
