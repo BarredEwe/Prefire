aaeda1869400207fecf8c65b30814c576f77ddeb
BarredEwe <barredewe@gmail.com>
2023-06-28 14:58:18 +0300

    Fix getting setting for snapshots

--- Example/PrefireExampleTests/PreviewTests.generated.swift
+++ Example/PrefireExampleTests/PreviewTests.generated.swift
@@ -8,7 +8,7 @@ import XCTest
 import SwiftUI
 import Prefire
 
-import SnapshotTesting
+@testable import SnapshotTesting
 #if canImport(AccessibilitySnapshot)
     import AccessibilitySnapshot
 #endif
@@ -27,69 +27,67 @@ class PreviewTests: XCTestCase {
     }
 
     func test_authView_Preview() {
-        let previews = AuthView_Preview.previews
-        for (index, preview) in AuthView_Preview._allPreviews.enumerated() {
-            let settings = previews.snapshotSettings(for: index)
-            assertSnapshots(matching: preview, precision: settings.precision, delay: settings.delay)
+        for preview in AuthView_Preview._allPreviews {
+            assertSnapshots(matching: preview)
         }
     }
 
     func test_circleImage() {
-        let previews = CircleImage_Previews.previews
-        for (index, preview) in CircleImage_Previews._allPreviews.enumerated() {
-            let settings = previews.snapshotSettings(for: index)
-            assertSnapshots(matching: preview, precision: settings.precision, delay: settings.delay)
+        for preview in CircleImage_Previews._allPreviews {
+            assertSnapshots(matching: preview)
         }
     }
 
     func test_greenButton() {
-        let previews = GreenButton_Previews.previews
-        for (index, preview) in GreenButton_Previews._allPreviews.enumerated() {
-            let settings = previews.snapshotSettings(for: index)
-            assertSnapshots(matching: preview, precision: settings.precision, delay: settings.delay)
+        for preview in GreenButton_Previews._allPreviews {
+            assertSnapshots(matching: preview)
         }
     }
 
     func test_prefireView_Preview() {
-        let previews = PrefireView_Preview.previews
-        for (index, preview) in PrefireView_Preview._allPreviews.enumerated() {
-            let settings = previews.snapshotSettings(for: index)
-            assertSnapshots(matching: preview, precision: settings.precision, delay: settings.delay)
+        for preview in PrefireView_Preview._allPreviews {
+            assertSnapshots(matching: preview)
         }
     }
 
     func test_testViewWithoutState() {
-        let previews = TestViewWithoutState_Previews.previews
-        for (index, preview) in TestViewWithoutState_Previews._allPreviews.enumerated() {
-            let settings = previews.snapshotSettings(for: index)
-            assertSnapshots(matching: preview, precision: settings.precision, delay: settings.delay)
+        for preview in TestViewWithoutState_Previews._allPreviews {
+            assertSnapshots(matching: preview)
         }
     }
 
     func test_testView() {
-        let previews = TestView_Previews.previews
-        for (index, preview) in TestView_Previews._allPreviews.enumerated() {
-            let settings = previews.snapshotSettings(for: index)
-            assertSnapshots(matching: preview, precision: settings.precision, delay: settings.delay)
+        for preview in TestView_Previews._allPreviews {
+            assertSnapshots(matching: preview)
         }
     }
 
     // MARK: Private
 
-    private func assertSnapshots(matching preview: _Preview, precision: Float?, delay: TimeInterval?, testName: String = #function) {
+    private func assertSnapshots(matching preview: _Preview, testName: String = #function) {
         let isScreen = preview.layout == .device
         let device = preview.device?.snapshotDevice() ?? deviceConfig
-        var view = preview.content
-        view = isScreen ? view : AnyView(view.frame(width: device.size?.width).fixedSize(horizontal: false, vertical: true))
+        var delay: TimeInterval = 0
+        var precision: Float = 1
+
+        let view = preview.content
+            .onPreferenceChange(DelayPreferenceKey.self) { delay = $0 }
+            .onPreferenceChange(PrecisionPreferenceKey.self) { precision = $0 }
+
+        let matchingView = isScreen ? AnyView(view) : AnyView(view
+            .frame(width: device.size?.width)
+            .fixedSize(horizontal: false, vertical: true)
+        )
 
         assertSnapshot(
-            matching: view,
-            as: .wait(for: delay, on: .image(precision: precision ?? 1, layout: isScreen ? .device(config: device) : .sizeThatFits)),
+            matching: matchingView,
+            as: .prefireImage(precision: { precision }, duration: { delay }, layout: isScreen ? .device(config: device) : .sizeThatFits),
             named: preview.displayName,
             testName: testName
         )
+
         #if canImport(AccessibilitySnapshot)
-            let vc = UIHostingController(rootView: view)
+            let vc = UIHostingController(rootView: matchingView)
             vc.view.frame = UIScreen.main.bounds
             assertSnapshot(
                 matching: vc,
@@ -114,45 +112,12 @@ class PreviewTests: XCTestCase {
     }
 }
 
-private extension Mirror {
-    func findValue(for name: String) -> Any? {
-        var mirror: Mirror? = self
-
-        while mirror != nil {
-            guard let value = mirror?.children.first(where: { "\($0.value)".contains(name) })?.value else {
-                return mirror?.children.first?.value
-            }
-            mirror = Mirror(reflecting: value)
-        }
-
-        return nil
-    }
-
-    var values: [String: Any] {
-        children.reduce([String: Any]()) { partialResult, child in
-            var partialResult = partialResult
-            if let index = child.label {
-                partialResult[index] = child.value
-            }
-            return partialResult
-        }
-    }
-}
-
-private extension Snapshotting {
-    static func wait(for duration: TimeInterval?, on strategy: Snapshotting) -> Snapshotting {
-        if let duration {
-            return wait(for: duration, on: strategy)
-        } else {
-            return strategy
-        }
-    }
-}
+// MARK: - SnapshotTesting + Extensions
 
 private extension PreviewDevice {
     func snapshotDevice() -> ViewImageConfig? {
         switch rawValue {
-        case "iPhone 12", "iPhone 11", "iPhone 10":
+        case "iPhone 14", "iPhone 13", "iPhone 12", "iPhone 11", "iPhone 10":
             return .iPhoneX
         case "iPhone 6", "iPhone 6s", "iPhone 7", "iPhone 8":
             return .iPhone8
@@ -165,17 +130,75 @@ private extension PreviewDevice {
     }
 }
 
-private extension View {
-    func snapshotSettings(for index: Int) -> (precision: Float?, delay: TimeInterval?) {
-        let viewValue = Mirror(reflecting: self).children.first?.value
-        let viewArray = viewValue.flatMap { Mirror(reflecting: $0).values }
+private extension Snapshotting where Value: SwiftUI.View, Format == UIImage {
+    static func prefireImage(
+        drawHierarchyInKeyWindow: Bool = false,
+        precision: @escaping () -> Float,
+        duration: @escaping () -> TimeInterval,
+        layout: SwiftUISnapshotLayout = .sizeThatFits,
+        traits: UITraitCollection = .init()
+    ) -> Snapshotting {
+        let config: ViewImageConfig
 
-        let precision = Mirror(reflecting: viewArray?[".\(index)"] as Any)
-            .findValue(for: String(describing: PrecisionPreferenceKey.self)) as? Float
+        switch layout {
+        #if os(iOS) || os(tvOS)
+        case let .device(config: deviceConfig):
+            config = deviceConfig
+        #endif
+        case .sizeThatFits:
+            config = .init(safeArea: .zero, size: nil, traits: traits)
+        case let .fixed(width: width, height: height):
+            let size = CGSize(width: width, height: height)
+            config = .init(safeArea: .zero, size: size, traits: traits)
+        }
 
-        let delay = Mirror(reflecting: viewArray?[".\(index)"] as Any)
-            .findValue(for: String(describing: DelayPreferenceKey.self)) as? TimeInterval
+        return SimplySnapshotting<UIImage>(pathExtension: "png", diffing: .prefireImage(precision: precision, scale: traits.displayScale))
+            .asyncPullback { view in
+                var config = config
 
-        return (precision, delay)
+                let controller: UIViewController
+
+                if config.size != nil {
+                    controller = UIHostingController(rootView: view)
+                } else {
+                    let hostingController = UIHostingController(rootView: view)
+
+                    let maxSize = CGSize.zero
+                    config.size = hostingController.sizeThatFits(in: maxSize)
+
+                    controller = hostingController
+                }
+
+                return Async<UIImage> { callback in
+                    let strategy = snapshotView(
+                        config: config,
+                        drawHierarchyInKeyWindow: drawHierarchyInKeyWindow,
+                        traits: traits,
+                        view: controller.view,
+                        viewController: controller
+                    )
+
+                    let duration = duration()
+                    if duration != .zero {
+                        let expectation = XCTestExpectation(description: "Wait")
+                        DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
+                            expectation.fulfill()
+                        }
+                        _ = XCTWaiter.wait(for: [expectation], timeout: duration + 1)
+                    }
+                    strategy.run(callback)
+                }
+            }
+    }
+}
+
+private extension Diffing where Value == UIImage {
+    static func prefireImage(precision: @escaping () -> Float, scale: CGFloat?) -> Diffing {
+        lazy var originalDiffing = Diffing.image(precision: precision(), scale: scale)
+        return Diffing(
+            toData: { originalDiffing.toData($0) },
+            fromData: { originalDiffing.fromData($0) },
+            diff: { originalDiffing.diff($0, $1) }
+        )
     }
 }
