a18d3aa6f9240858ea66544cbd5de86d9487d7c4
BarredEwe <barredewe@gmail.com>
2024-04-05 12:52:53 +0300

    Add SwiftPM example

---
+++ Example/Package.swift
@@ -0,0 +1,45 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PrefireExample",
+    platforms: [.iOS("17")],
+    products: [
+        .library(
+            name: "PrefireExample",
+            targets: ["PrefireExample"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            name: "Prefire",
+            path: "../"
+        ),
+        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.15.4")
+    ],
+    targets: [
+        .target(
+            name: "PrefireExample",
+            dependencies: [
+                "Prefire"
+            ],
+            path: "Shared",
+            plugins: [
+                .plugin(name: "PrefirePlaybookPlugin", package: "Prefire")
+            ]
+        ),
+        .testTarget(
+            name: "PrefireExampleTests",
+            dependencies: [
+                "PrefireExample",
+                .product(name: "SnapshotTesting", package: "swift-snapshot-testing"),
+            ],
+            path: "PrefireExampleTests",
+            plugins: [
+                .plugin(name: "PrefireTestsPlugin", package: "Prefire")
+            ]
+        )
+    ]
+)
