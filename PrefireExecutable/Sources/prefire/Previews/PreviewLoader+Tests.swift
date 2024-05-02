e7e9a909881dd61444f3fb7280d02e009e0536f0
BarredEwe <barredewe@gmail.com>
2024-05-02 19:15:19 +0300

    Processing test's function name

--- PrefireExecutable/Sources/prefire/Previews/PreviewLoader+Tests.swift
+++ PrefireExecutable/Sources/prefire/Previews/PreviewLoader+Tests.swift
@@ -1,10 +1,12 @@
 import Foundation
 
 extension PreviewLoader {
+    private static let funcCharacterSet = CharacterSet(arrayLiteral: "_").inverted.intersection(.alphanumerics.inverted)
+
     static func loadPreviewBodies(for target: String, and sources: [String], defaultEnabled: Bool) -> String? {
         guard let findedBodies = loadRawPreviewBodies(for: target, and: sources, defaultEnabled: true) else { return nil }
 
-        let result = findedBodies.map { makeFunc(body: $0) + "\r\n" }.reduce("", +)
+        let result = findedBodies.map { makeFunc(body: $0) + "\r\n" }.joined()
 
         return result
     }
@@ -12,9 +14,10 @@ extension PreviewLoader {
     private static func makeFunc(body: String) -> String {
         let rawPreviewModel = RawPreviewModel(from: body)
         let isScreen = rawPreviewModel.traits == ".device"
+        let componentTestName = rawPreviewModel.displayName.components(separatedBy: funcCharacterSet).joined()
 
         return """
-                func test_\(rawPreviewModel.displayName)_Preview() {
+                func test_\(componentTestName)_Preview() {
                     let preview = {
             \(rawPreviewModel.body)
                     }
