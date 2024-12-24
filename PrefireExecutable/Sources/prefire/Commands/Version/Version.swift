1e049c1b6702c6404b3517e70f12b05f2fc0c9ba
BarredEwe <barredewe@gmail.com>
2024-12-24 10:28:50 +0300

    Version update to 3.1.0

--- PrefireExecutable/Sources/prefire/Commands/Version/Version.swift
+++ PrefireExecutable/Sources/prefire/Commands/Version/Version.swift
@@ -5,7 +5,7 @@ extension Prefire {
     struct Version: ParsableCommand {
         static let configuration = CommandConfiguration(abstract: "Display the current version of Prefire")
 
-        static var value: String = "3.0.5"
+        static var value: String = "3.1.0"
 
         func run() throws {
             print(Self.value)
