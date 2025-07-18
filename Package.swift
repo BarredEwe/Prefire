1a8ed63ce6bd4c4b09fddfd65c1ef0a53c6a71f6
BarredEwe <barredewe@gmail.com>
2025-07-18 15:22:43 +0300

    Mint supporting

--- Package.swift
+++ Package.swift
@@ -19,6 +19,10 @@ let package = Package(
             name: "PrefireTestsPlugin",
             targets: ["PrefireTestsPlugin"]
         ),
+        .executable(
+            name: "prefire",
+            targets: ["PrefireBinary"]
+        ),
     ],
     targets: [
         .target(
